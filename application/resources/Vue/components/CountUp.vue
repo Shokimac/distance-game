759112813764c27df3c007cce0b7bbf4986ef1fd
Shoki Yokomizo <64714255+Shokimac@users.noreply.github.com>
2023-10-15 12:28:55 +0900

    feat: #1 vue-router導入

--- application/resources/Vue/components/CountUp.vue
+++ application/resources/Vue/components/CountUp.vue
@@ -10,7 +10,7 @@ const counter = ref(0)
 
     <button
       class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-      @click="counter += 1">
+      @click="counter += 10">
       クリックしてね
     </button>
   </div>
--- application/resources/js/app.js
+++ application/resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import '../css/app.css';
-import { createApp } from 'vue'
-import App from '../ts/App.vue'
-createApp(App).mount("#app")
+import { createApp } from 'vue';
+import router from './router'
+import App from '../Vue/App.vue';
+createApp(App).use(router).mount("#app")
