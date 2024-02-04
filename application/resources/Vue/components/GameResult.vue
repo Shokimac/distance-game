8865e033c13e1a3b998e2c95dec5d45ce0d99043
Shoki Yokomizo <64714255+Shokimac@users.noreply.github.com>
2024-02-04 16:23:08 +0900

    #4 スタイル微調整

--- application/resources/Vue/components/GameResult.vue
+++ application/resources/Vue/components/GameResult.vue
@@ -20,13 +20,14 @@ import SubmitButton from './ViewParts/SubmitButton.vue';
           </p>
         </div>
         <div class="flex w-full mt-2">
-          <div class="w-12">
+          <div class="w-2/12">
             <img :src="`/assets/icons/rank_flag_2.svg`" :alt="`2着ランクアイコン`" class="inline mr-4 max-w-full">
           </div>
-          <div class="w-full flex">
+          <div class="w-10/12 flex">
             <div class="w-1/2 font-bold text-center">
               <span class="text-lg">〒</span><span class="text-forest text-3xl font-din">578</span><span
                 class="font-extrabold text-xl">-</span><span class="text-forest text-3xl font-din">0983</span>
+              <p class="truncate">福島県福島市下谷寺</p>
             </div>
             <div class="w-1/2 flex flex-col">
               <div class="w-full text-right font-bold">
