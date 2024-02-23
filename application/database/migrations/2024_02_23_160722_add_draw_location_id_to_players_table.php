69b5135b3e7b3ca84f02e4e6df24206d6f63972b
Shoki Yokomizo <64714255+Shokimac@users.noreply.github.com>
2024-02-23 16:14:21 +0900

    #3 playersTBLに入れ忘れてたカラム追加とデータ型を一部変更

---
+++ application/database/migrations/2024_02_23_160722_add_draw_location_id_to_players_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->string('draw_location_id', 50)->after('turn')->comment('スロットで引いた場所')->references('id')->on('location');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropColumn('draw_location_id');
+        });
+    }
+};
